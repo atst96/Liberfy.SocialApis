b136107b78db7257da3690451f26f14444fec254
atst_96 <tnkjpei04@gmail.com>
2021-08-13 21:36:31 +0900

    add: テスト用にUtilクラス

---
+++ SocialApis.Test/TestUtils/TestJsonUtil.cs
@@ -0,0 +1,29 @@
+﻿namespace SocialApis.Test.TestUtils
+{
+    internal static class TestJsonUtil
+    {
+        /// <summary>
+        /// JSON文字列を生成する
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static string CreateSingleJsonObject(string key, string value)
+        {
+            var jsonValue = value is null ? "null" : value;
+            return $"{{\"{key}\":{jsonValue}}}";
+        }
+
+        /// <summary>
+        /// JSON文字列を生成する
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static string CreateSingleJsonString(string key, string value)
+        {
+            var jsonValue = value is null ? "null" : $"\"{value}\"";
+            return $"{{\"{key}\":{jsonValue}}}";
+        }
+    }
+}
