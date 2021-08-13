7891e1583de41868d8fc5cb4d883b6dfa2d13bf7
atst_96 <tnkjpei04@gmail.com>
2021-08-13 21:36:03 +0900

    add: フォーマッタ関連のテストクラス

---
+++ SocialApis.Test/Formatters/StringIntFormatterTest.cs
@@ -0,0 +1,86 @@
+﻿using SocialApis.Formatters;
+using SocialApis.Test.TestUtils;
+using SocialApis.Utils;
+using System.Runtime.Serialization;
+using Utf8Json;
+using Xunit;
+
+namespace SocialApis.Test.Formatters
+{
+    /// <summary>
+    /// <see cref="StringIntFormatter"/>のテスト
+    /// </summary>
+    public class StringIntFormatterTest
+    {
+        public class IntClass
+        {
+            [DataMember(Name = "value", Order = 1)]
+            [JsonFormatter(typeof(StringIntFormatter))]
+            public int? Value { get; set; }
+        }
+
+        public class NullableIntClass
+        {
+            [DataMember(Name = "value")]
+            [JsonFormatter(typeof(StringIntFormatter))]
+            public int? Value { get; set; }
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(100)]
+        [InlineData(2147483647)]
+        [InlineData(-2147483648)]
+        public void TestDeserialize1(int expected)
+        {
+            // {"value": ${expected}}
+            var json = TestJsonUtil.CreateSingleJsonObject(key: "value", value: expected.ToString());
+            var actual = JsonUtil.Deserialize<IntClass>(json).Value;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(100)]
+        [InlineData(2147483647)]
+        [InlineData(-2147483648)]
+        public void TestDeserializeNullableString(int expected)
+        {
+            var json = TestJsonUtil.CreateSingleJsonObject(key: "value", value: expected.ToString());
+            var actual = JsonSerializer.Deserialize<IntClass>(json).Value;
+
+            Assert.Equal(expected, actual);
+        }
+
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData(0)]
+        [InlineData(100)]
+        [InlineData(2147483647)]
+        [InlineData(-2147483648)]
+        public void TestDeserialize2(int? expected)
+        {
+            // {"value": ${expected}}
+            var json = TestJsonUtil.CreateSingleJsonObject(key: "value", value: expected?.ToString());
+            var actual = JsonUtil.Deserialize<NullableIntClass>(json).Value;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData(0)]
+        [InlineData(100)]
+        [InlineData(2147483647)]
+        [InlineData(-2147483648)]
+        public void TestDeserializeNullableString2(int? expected)
+        {
+            var json = TestJsonUtil.CreateSingleJsonObject(key: "value", value: expected?.ToString());
+            var actual = JsonSerializer.Deserialize<NullableIntClass>(json).Value;
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ SocialApis.Test/Formatters/StringLogFormatterTest.cs
@@ -0,0 +1,86 @@
+﻿using SocialApis.Formatters;
+using SocialApis.Test.TestUtils;
+using SocialApis.Utils;
+using System.Runtime.Serialization;
+using Utf8Json;
+using Xunit;
+
+namespace SocialApis.Test.Formatters
+{
+    /// <summary>
+    /// <see cref="StringLongFormatter"/>のテスト
+    /// </summary>
+    public class StringLogFormatterTest
+    {
+        public class LongClass
+        {
+            [DataMember(Name = "value")]
+            [JsonFormatter(typeof(StringLongFormatter))]
+            public long Value { get; set; }
+        }
+
+        public class NullableLongClass
+        {
+            [DataMember(Name = "value")]
+            [JsonFormatter(typeof(StringLongFormatter))]
+            public long? Value { get; set; }
+        }
+
+        [Theory]
+        [InlineData(0L)]
+        [InlineData(100L)]
+        [InlineData(long.MaxValue)]
+        [InlineData(long.MinValue)]
+        public void TestDeserialize1(long expected)
+        {
+            // {"value": ${expected}}
+            var json = TestJsonUtil.CreateSingleJsonObject(key: "value", value: expected.ToString());
+            var actual = JsonUtil.Deserialize<LongClass>(json).Value;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(0L)]
+        [InlineData(100L)]
+        [InlineData(long.MaxValue)]
+        [InlineData(long.MinValue)]
+        public void TestDeserializeNullableString(long expected)
+        {
+            var json = TestJsonUtil.CreateSingleJsonObject(key: "value", value: expected.ToString());
+            var actual = JsonSerializer.Deserialize<LongClass>(json).Value;
+
+            Assert.Equal(expected, actual);
+        }
+
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData(0L)]
+        [InlineData(100L)]
+        [InlineData(long.MaxValue)]
+        [InlineData(long.MinValue)]
+        public void TestDeserialize2(long? expected)
+        {
+            // {"value": ${expected}}
+            var json = TestJsonUtil.CreateSingleJsonObject(key: "value", value: expected?.ToString());
+            var actual = JsonUtil.Deserialize<NullableLongClass>(json).Value;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData(0L)]
+        [InlineData(100L)]
+        [InlineData(long.MaxValue)]
+        [InlineData(long.MinValue)]
+        public void TestDeserializeNullableString2(long? expected)
+        {
+            var json = TestJsonUtil.CreateSingleJsonObject(key: "value", value: expected?.ToString());
+            var actual = JsonSerializer.Deserialize<NullableLongClass>(json).Value;
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
